97a785bc2dfeec4e2d6aa28cf22c40671fcfbfd5
rajvardhan97 <n.rajvardhan2627@gmail.com>
2022-06-10 09:22:25 +0530

    [Raj]Add: Workshop Program to find Nth Harmonic Number

---
+++ HarmonicNumber_Workshop/HarmonicNumber_Workshop/HarmonicNumber.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarmonicNumber_Workshop
+{
+    internal class HarmonicNumber
+    {
+        public void Harmonic()
+        {
+            float value = 0;
+            Console.WriteLine("Enter a number to find Harmonic Number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            while(number == 0)
+            {
+                Console.WriteLine("Please enter a number which is not zero");
+            }   number = Convert.ToInt32(Console.ReadLine());
+
+            for (float i = 1; i <= number; i++)
+            {
+                value += 1 / i;
+            }
+            Console.WriteLine("The  Nth harmonic Number is " + value);
+        }
+    }      
+}
---
+++ HarmonicNumber_Workshop/HarmonicNumber_Workshop/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarmonicNumber_Workshop
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Workshop program for harmonic number");
+            HarmonicNumber harmonicNumber = new HarmonicNumber();
+            harmonicNumber.Harmonic();
+        }
+    }
+}
